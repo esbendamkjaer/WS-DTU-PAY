10bfebeb5cf3c5a561aba028960742e6275aa361
Alexander Matzen <git@alexander.dk>
2024-01-06 14:35:19 +0100

    Add sampletest to confirm surefire

---
+++ dtu-pay-client/src/test/java/dk/dtu/grp08/SampleTest.java
@@ -0,0 +1,11 @@
+package dk.dtu.grp08;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+    @Test
+    public void test() {
+        Assert.assertSame(true, true);
+    }
+}
