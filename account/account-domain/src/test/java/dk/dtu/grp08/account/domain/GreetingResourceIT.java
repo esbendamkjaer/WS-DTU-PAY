5e4055ef7979f859469bb55fcc2f52aa17834969
Alexander Matzen <git@alexander.dk>
2024-01-09 14:11:03 +0100

    Add new account subprojects

---
+++ account/account-domain/src/test/java/dk/dtu/grp08/account/domain/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package dk.dtu.grp08.account.domain;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
