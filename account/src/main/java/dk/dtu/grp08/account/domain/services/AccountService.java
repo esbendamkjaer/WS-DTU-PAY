99daf8b38e8d562b154fe4ace96731d3143aa6ff
Alexander Matzen <git@alexander.dk>
2024-01-11 15:09:33 +0100

    WIP: Inject repo into service

--- account/src/main/java/dk/dtu/grp08/account/domain/services/AccountService.java
+++ account/src/main/java/dk/dtu/grp08/account/domain/services/AccountService.java
@@ -1,10 +1,14 @@
 package dk.dtu.grp08.account.domain.services;
 
 import dk.dtu.grp08.account.domain.models.UserAccount;
+import dk.dtu.grp08.account.domain.repository.IAccountRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class AccountService {
+    @Inject
+    IAccountRepository accountRepository;
 
     public UserAccount registerAccount(
         String firstName,
@@ -20,6 +24,10 @@ public class AccountService {
         );
 
         return userAccount;
+    }
+
+    public UserAccount getUserAccountById(String id) {
+        return accountRepository.findById(id);
     }
 
 }
