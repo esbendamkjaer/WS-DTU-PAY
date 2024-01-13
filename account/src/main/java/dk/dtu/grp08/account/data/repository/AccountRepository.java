c1d22afa616eada700ef84c4b2dc65ed92f83aec
Alexander Matzen <git@alexander.dk>
2024-01-13 13:47:30 +0100

    Add more functions for repository

--- account/src/main/java/dk/dtu/grp08/account/data/repository/AccountRepository.java
+++ account/src/main/java/dk/dtu/grp08/account/data/repository/AccountRepository.java
@@ -1,19 +1,43 @@
 package dk.dtu.grp08.account.data.repository;
 
+import dk.dtu.grp08.account.domain.models.UserAccountId;
+import dk.dtu.grp08.account.domain.models.UserAccount;
 import dk.dtu.grp08.account.domain.repository.IAccountRepository;
 import jakarta.enterprise.context.ApplicationScoped;
-import dk.dtu.grp08.account.domain.models.UserAccount;
+import lombok.val;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @ApplicationScoped
 public class AccountRepository implements IAccountRepository {
-    private final Map<String, UserAccount> userAccounts = new HashMap<>();
+    private final Map<UserAccountId, UserAccount> userAccounts = new HashMap<>();
 
     @Override
-    public UserAccount findById(String id) {
-        return null;
+    public UserAccount createUserAccount(UserAccount userAccount) {
+        val id = userAccount.getId();
+        userAccounts.put(id, userAccount);
+        return userAccount;
+    }
+
+    @Override
+    public void delete(UserAccount userAccount) {
+        userAccounts.remove(userAccount.getId());
+    }
+
+    @Override
+    public void delete(UserAccountId userAccountId) {
+        userAccounts.remove(userAccountId);
+    }
+
+    @Override
+    public UserAccount findById(UserAccountId id) {
+        return userAccounts.get(id);
+    }
+
+    @Override
+    public List<UserAccount> findAll() {
+        return (List<UserAccount>) userAccounts.values();
     }
 }
--- account/src/main/java/dk/dtu/grp08/account/domain/repository/IAccountRepository.java
+++ account/src/main/java/dk/dtu/grp08/account/domain/repository/IAccountRepository.java
@@ -1,7 +1,20 @@
 package dk.dtu.grp08.account.domain.repository;
 
 import dk.dtu.grp08.account.domain.models.UserAccount;
+import dk.dtu.grp08.account.domain.models.UserAccountId;
+
+import java.util.List;
 
 public interface IAccountRepository {
-    UserAccount findById(String id);
+
+    UserAccount createUserAccount(UserAccount userAccount);
+
+    void delete(UserAccount userAccount);
+
+    void delete(UserAccountId userAccountId);
+
+    UserAccount findById(UserAccountId id);
+
+    List<UserAccount> findAll();
+
 }
