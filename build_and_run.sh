2e6bb6cb75e46a0c949ef775288f30999c98b9fc
Alexander Matzen <git@alexander.dk>
2024-01-15 10:51:45 +0100

    Redo description

--- build_and_run.sh
+++ build_and_run.sh
@@ -31,7 +31,7 @@ popd
 # Build and start Docker containers
 # -d is detached mode, allows the service to continue even after the shell process is complete
 # --build is to build images before starting containers
-# --wait is to wait for the startup of the containers, used instead of a sleep with a defined timer
+# --wait makes the command return/exit only after all containers have been started
 docker-compose up -d --build --wait
 
 #run test for all microservices
