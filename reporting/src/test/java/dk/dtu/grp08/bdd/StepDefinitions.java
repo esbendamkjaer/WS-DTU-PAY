805564a2098dea3b6584a3ed370264f6254b644e
Muhamad Nadali <muhamadsdu@gmail.com>
2024-01-16 14:33:49 +0100

    reporting

--- reporting/src/test/java/dk/dtu/grp08/bdd/StepDefinitions.java
+++ reporting/src/test/java/dk/dtu/grp08/bdd/StepDefinitions.java
@@ -1,29 +1,96 @@
 package dk.dtu.grp08.bdd;
 
-import io.cucumber.java.After;
-import io.cucumber.java.en.And;
+import dk.dtu.grp08.adapter.BankAdapter;
+import dk.dtu.grp08.adapter.IBankAdapter;
+import dk.dtu.grp08.dtupay.customer.CustomerFacade;
+import dk.dtu.grp08.dtupay.customer.ICustomerFacade;
+import dk.dtu.grp08.dtupay.merchant.IMerchantFacade;
+import dk.dtu.grp08.dtupay.merchant.MerchantFacade;
+import dk.dtu.grp08.dtupay.models.PaymentRequest;
+import dk.dtu.grp08.dtupay.models.Token;
+import dk.dtu.grp08.reporting.domain.models.user.UserAccount;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import jakarta.ws.rs.ClientErrorException;
+import messaging.Event;
 import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-
-import static org.junit.Assert.assertTrue;
 
 public class StepDefinitions {
 
 
+    private final List<Token> customerTokens = new ArrayList<>();
+    private final IBankAdapter bankAdapter = new BankAdapter();
+    private final IMerchantFacade merchantFacade = new MerchantFacade();
+    private final ICustomerFacade customerFacade = new CustomerFacade();
+    private UserAccount customer;
+    private UserAccount merchant;
+    private Transaction paymentTransferredEvent;
+    private Report customerReport;
+    private Report merchantReport;
 
+    private PaymentRequest paymentRequest;
 
+    @Given("A \"PAYMENT_TRANSFERRED\" event")
+    public void aPaymentTransferredEvent(double amount) {
 
+        customer = new UserAccount();
+        customer.setFirstName("Hubert Jens");
+        merchant = new UserAccount();
+        merchant.setFirstName("Hans Peter");
 
+        this.paymentRequest = new PaymentRequest();
+        paymentRequest.setAmount(
+                BigDecimal.valueOf(amount)
+        );
+        paymentRequest.setMerchantId(
+                this.merchant.getId().getId()
+        );
 
+        this.paymentRequest.setToken(
+                this.customerTokens.removeLast()
+        );
 
+        this.merchantFacade.pay(
+                this.paymentRequest
+        );
+
+    }
+
+    @When("the customer requests a report")
+    public void theCustomerRequestsAReport() {
+        customerReport = customerFacade.requestReport(customer.getUserId());
+    }
+
+    @Then("the customer should see a report with the following transaction details")
+    public void theCustomerShouldSeeAReportWithTheFollowingTransactionDetails() {
+
+        Assert.assertTrue(customerReport.containsTransaction(paymentTransferredEvent));
+    }
+
+    @When("the merchant requests a report")
+    public void theMerchantRequestsAReport() {
+        merchantReport = merchantFacade.requestReport(merchant.getUserId());
+    }
+
+    @Then("the merchant should see a report with the following transaction details")
+    public void theMerchantShouldSeeAReportWithTheFollowingTransactionDetails() {
+
+        Assert.assertTrue(merchantReport.containsTransaction(paymentTransferredEvent));
+    }
+
+    class Transaction {
+
+    }
+
+    class Report {
+
+        boolean containsTransaction(Transaction transaction) {
+
+            return true;
+        }
+    }
 }
-
